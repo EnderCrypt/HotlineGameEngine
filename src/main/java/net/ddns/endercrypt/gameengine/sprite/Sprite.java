95047a7cef19b91ff5e150321c47aa5f3b0accb3
Magnus <EnderCrypt@gmail.com>
2018-05-01 22:34:03 +0200

    implemented sprites

--- src/main/java/net/ddns/endercrypt/gameengine/sprite/Sprite.java
+++ src/main/java/net/ddns/endercrypt/gameengine/sprite/Sprite.java
@@ -1,5 +1,8 @@
 package net.ddns.endercrypt.gameengine.sprite;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.Serializable;
 
 public class Sprite implements Serializable
@@ -9,5 +12,29 @@ public class Sprite implements Serializable
 	 * 
 	 */
 
-	// TODO: implement
+	private File file;
+
+	private transient BufferedImage image;
+
+	public Sprite(File file)
+	{
+		this.file = file;
+	}
+
+	public File getFile()
+	{
+		return file;
+	}
+
+	public void draw(Graphics2D g2d, int x, int y)
+	{
+		if (image == null)
+		{
+			image = SpriteManager.getImage(file);
+		}
+		else
+		{
+			g2d.drawImage(image, x, y, null);
+		}
+	}
 }
--- src/main/java/net/ddns/endercrypt/gameengine/sprite/SpriteManager.java
+++ src/main/java/net/ddns/endercrypt/gameengine/sprite/SpriteManager.java
@@ -1,6 +1,89 @@
 package net.ddns.endercrypt.gameengine.sprite;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.imageio.ImageIO;
+
 public class SpriteManager
 {
-	// TODO: implement
+	private static ExecutorService executor = Executors.newCachedThreadPool();
+
+	private static volatile boolean isLoading = false;
+	private static BlockingQueue<File> loadQueue = new ArrayBlockingQueue<>(100);
+
+	private static Map<File, BufferedImage> images = new HashMap<>();
+
+	static
+	{
+		executor.execute(new SpriteLoader());
+	}
+
+	public static boolean isLoading()
+	{
+		return isLoading;
+	}
+
+	public static BufferedImage getImage(File file)
+	{
+		BufferedImage image = images.get(file);
+		if (image == null)
+		{
+			if (loadQueue.contains(file) == false)
+			{
+				try
+				{
+					loadQueue.put(file);
+				}
+				catch (InterruptedException e)
+				{
+					// continue
+				}
+			}
+		}
+		return image;
+	}
+
+	private static class SpriteLoader implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			try
+			{
+				while (true)
+				{
+					if (loadQueue.isEmpty())
+					{
+						isLoading = false;
+					}
+					File file = loadQueue.take();
+					isLoading = true;
+					BufferedImage image = null;
+					try
+					{
+						image = ImageIO.read(file);
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+						continue;
+					}
+
+					images.put(file, image);
+				}
+			}
+			catch (InterruptedException e)
+			{
+				return;
+			}
+		}
+	}
 }
