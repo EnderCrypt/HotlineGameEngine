d315876743516d944e09742c0dbd9c0222a44239
Magnus <EnderCrypt@gmail.com>
2018-05-02 10:05:12 +0200

    made more tests to test new functionality

--- src/test/java/net/ddns/endercrypt/Main.java
+++ src/test/java/net/ddns/endercrypt/Main.java
@@ -1,13 +1,19 @@
 package net.ddns.endercrypt;
 
+import java.io.FileNotFoundException;
+
 import net.ddns.endercrypt.gameengine.HotlineGameEngine;
+import net.ddns.endercrypt.gameengine.entities.sprite.SpriteManager;
 
 public class Main
 {
 	private static HotlineGameEngine hotlineGameEngine;
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		hotlineGameEngine = new HotlineGameEngine("Lord Knows");
+		SpriteManager.loadImage("res/science.png");
+
+		hotlineGameEngine = new HotlineGameEngine("Game test");
+		hotlineGameEngine.getRoomManager().startRoom(new TestEntity());
 	}
 }
