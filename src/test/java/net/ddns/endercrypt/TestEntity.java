7a91e1ec9a1022ccfec43e3c43e972191ac9bda0
Magnus <EnderCrypt@gmail.com>
2018-05-02 10:07:08 +0200

    added test entity for testing

---
+++ src/test/java/net/ddns/endercrypt/TestEntity.java
@@ -0,0 +1,29 @@
+package net.ddns.endercrypt;
+
+import net.ddns.endercrypt.gameengine.entities.GameEntity;
+import net.ddns.endercrypt.gameengine.entities.sprite.Sprite;
+
+public class TestEntity extends GameEntity
+{
+	private static final long serialVersionUID = -3101929237048563872L;
+
+	/**
+	 * 
+	 */
+
+	@Override
+	protected void onCreate()
+	{
+		sprite = Sprite.get("res/science.png");
+
+		position.x = 100;
+		position.y = 100;
+	}
+
+	@Override
+	public void update()
+	{
+		super.update();
+		position.x += 1;
+	}
+}
