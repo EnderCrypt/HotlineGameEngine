f98136d7759f890a71e17cd8be61b8d5d6dbd3c7
Magnus <EnderCrypt@gmail.com>
2018-05-02 10:06:47 +0200

    added exception class for sprites (file not found)

---
+++ src/main/java/net/ddns/endercrypt/gameengine/entities/sprite/SpriteNotFound.java
@@ -0,0 +1,35 @@
+package net.ddns.endercrypt.gameengine.entities.sprite;
+
+public class SpriteNotFound extends RuntimeException
+{
+
+	public SpriteNotFound()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public SpriteNotFound(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SpriteNotFound(Throwable cause)
+	{
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SpriteNotFound(String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SpriteNotFound(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
