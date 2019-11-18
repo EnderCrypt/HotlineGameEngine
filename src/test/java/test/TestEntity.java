265fcfe2113251825810336d66ccb4b5253b4613
EnderCrypt <EnderCrypt@gmail.com>
2019-11-18 02:24:03 +0100

    fixed test entity

--- src/test/java/test/TestEntity.java
+++ src/test/java/test/TestEntity.java
@@ -1,29 +1,31 @@
 package test;
 
+
 import java.awt.event.KeyEvent;
 
 import endercrypt.hotline.engine.entities.GameEntity;
 import endercrypt.hotline.engine.sprite.Sprite;
 
+
 public class TestEntity extends GameEntity
 {
 	private static final long serialVersionUID = -3101929237048563872L;
-
+	
 	/**
 	 * 
 	 */
-
+	
 	private static final double SPEED = 1.0;
-
+	
 	@Override
 	protected void onCreate()
 	{
-		sprite = Sprite.get("res/science.png");
-
+		sprite = new Sprite("res/science.png");
+		
 		position.x = 100;
 		position.y = 100;
 	}
-
+	
 	@Override
 	protected void onKeyHold(int keyCode, boolean shift, boolean ctrl, boolean alt, boolean meta)
 	{
@@ -49,13 +51,13 @@ public class TestEntity extends GameEntity
 			break;
 		}
 	}
-
+	
 	@Override
 	public void onUpdate()
 	{
 		motion.multiplyLength(0.8);
 		rotation += 2.5;
-
+		
 		// getRoomContext().getView().position().set(position.x, position.y);
 	}
 }
