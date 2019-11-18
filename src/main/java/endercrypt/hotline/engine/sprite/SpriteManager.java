477636147cc2d91399ab07216b602fd6a9806f3a
EnderCrypt <EnderCrypt@gmail.com>
2019-11-18 02:04:06 +0100

    made sprites be loaded in as hardware accalerated images

--- src/main/java/endercrypt/hotline/engine/sprite/SpriteManager.java
+++ src/main/java/endercrypt/hotline/engine/sprite/SpriteManager.java
@@ -1,6 +1,11 @@
 package endercrypt.hotline.engine.sprite;
 
 
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,6 +20,10 @@ import javax.imageio.ImageIO;
 
 public class SpriteManager
 {
+	private static GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	private static GraphicsDevice graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
+	private static GraphicsConfiguration graphicsConfiguration = graphicsDevice.getDefaultConfiguration();
+	
 	private static Map<Path, BufferedImage> images = new HashMap<>();
 	
 	public static void registerImage(String file) throws SpriteException, IOException
@@ -34,7 +43,13 @@ public class SpriteManager
 			throw new SpriteException("Sprite already added");
 		}
 		
-		images.put(file, ImageIO.read(file.toFile()));
+		BufferedImage image = ImageIO.read(file.toFile());
+		BufferedImage compatibleImage = graphicsConfiguration.createCompatibleImage(image.getWidth(), image.getHeight(), Transparency.TRANSLUCENT);
+		Graphics2D g2d = compatibleImage.createGraphics();
+		g2d.drawImage(image, 0, 0, null);
+		g2d.dispose();
+		
+		images.put(file, compatibleImage);
 	}
 	
 	public static BufferedImage getImage(Path file)
