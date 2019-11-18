bbe4a278b245f9b08dbe9ed138b6eb415072f02a
EnderCrypt <EnderCrypt@gmail.com>
2019-11-18 03:42:50 +0100

    removed unused throws exceptions

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -1,19 +1,22 @@
 package test;
 
+
 import java.io.IOException;
 
 import endercrypt.hotline.engine.engine.HotlineGameEngine;
+import endercrypt.hotline.engine.sprite.SpriteException;
 import endercrypt.hotline.engine.sprite.SpriteManager;
+
 
 public class Main
 {
 	public static HotlineGameEngine hotlineGameEngine;
-
-	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException
+	
+	public static void main(String[] args) throws SpriteException, IOException
 	{
 		SpriteManager.registerImage("res/bomb.png").setCenter(128, 128);
 		SpriteManager.registerImage("res/science.png").setCenter(16, 16);
-
+		
 		hotlineGameEngine = new HotlineGameEngine("Game test");
 		hotlineGameEngine.getRoomManager().startRoom(new TestEntity());
 	}
